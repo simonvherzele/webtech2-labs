ff2cfbd6d6653572277f86efe0748cd7cdc84b89
simonvherzele <simon.vanherzele@gmail.com>
2017-03-16 19:42:23 +0100

    made class and constructor

--- es6/scripts/app.es6.js
+++ es6/scripts/app.es6.js
@@ -1,39 +1,40 @@
-function CardApp(){
-    this.buttonAddNote = document.getElementById("btnAddNote");
-    this.notesContainer = document.querySelector(".notes");
-    this.noteInput = document.querySelector("#txtAddNote");
-    
-    this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
-}
+class CardApp(){
+    Constructor{
+        this.buttonAddNote = document.getElementById("btnAddNote");
+        this.notesContainer = document.querySelector(".notes");
+        this.noteInput = document.querySelector("#txtAddNote");
+        
+        this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
+    }
 
-CardApp.prototype.resetForm = function(){
-    this.noteInput.value = "";
-    this.noteInput.focus();
-}
+    CardApp.prototype.resetForm = function(){
+        this.noteInput.value = "";
+        this.noteInput.focus();
+    }
 
-CardApp.prototype.addNote = function(e){
-    var newNote = document.createElement("div"); 
-    newNote.setAttribute("class", "card"); 
-    newNote.innerHTML = "<p>" + this.noteInput.value + "</p>";
-    
-    var noteLink = document.createElement("a");
-    noteLink.setAttribute("class", "card-remove");
-    noteLink.innerHTML = "Remove";
-    noteLink.setAttribute("href", "#");
-    noteLink.addEventListener("click", this.removeNote.bind(this));
-    
-    newNote.appendChild(noteLink);
-    
-    this.notesContainer.appendChild(newNote);
-    this.resetForm();
-    console.log(e);
-}
+    CardApp.prototype.addNote = function(e){
+        var newNote = document.createElement("div"); //<div> </div>
+        newNote.setAttribute("class", "card"); // <div>
+        newNote.innerHTML = "<p>" + this.noteInput.value + "</p>";
+        
+        var noteLink = document.createElement("a");
+        noteLink.setAttribute("class", "card-remove");
+        noteLink.innerHTML = "Remove";
+        noteLink.setAttribute("href", "#");
+        noteLink.addEventListener("click", this.removeNote.bind(this));
+        
+        newNote.appendChild(noteLink);
+        
+        this.notesContainer.appendChild(newNote);
+        this.resetForm();
+        console.log(e);
+    }
 
-CardApp.prototype.removeNote = function (e) {
-    var noteToRemove = e.target.parentElement;
-    this.notesContainer.removeChild(noteToRemove);
-    e.preventDefault();
+    CardApp.prototype.removeNote = function (e) {
+        var noteToRemove = e.target.parentElement;
+        this.notesContainer.removeChild(noteToRemove);
+        e.preventDefault();
+    }
 }
-
 
 var myApp = new CardApp();
